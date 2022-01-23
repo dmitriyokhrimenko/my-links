f59c373b215f951de46d92f01525e2ddd91d1ff8
Dmitriy Okhrimenko <dm.okhrimenko@gmail.com>
2022-01-23 13:46:38 +0200

    fix(ci): test new ci

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          New changes with a new deployment flow:)))
+        </p>
+        <p>
           Happy deploy here <code>src/App.js</code> and save to reload.
         </p>
         <a
